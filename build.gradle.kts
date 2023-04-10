0cf95300cc8b80babe59d11607d10fa6535e7c05
Amitai Schleier <schmonz-web-whenalltestsweregreen@schmonz.com>
2023-04-10 06:39:30 -0400

    Remove unneeded comment.

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,7 +98,7 @@ publishing {
 
 nexusPublishing {
     repositories {
-        sonatype { // only for users registered in Sonatype after 24 Feb 2021
+        sonatype {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
