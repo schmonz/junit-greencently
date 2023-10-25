ecc8674084a79198092731e38202c58373633ee4
Amitai Schleier <schmonz-web-greencently@schmonz.com>
2023-10-25 05:49:49 -0400

    Bump Gradle, ben-manes.versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.10"
     jacoco
-    id("com.github.ben-manes.versions") version "0.48.0"
+    id("com.github.ben-manes.versions") version "0.49.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.1"
 
     // publishing
