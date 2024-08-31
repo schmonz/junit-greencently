448f50a7a25f787f20973d525f15845c521740e7
Amitai Schleier <schmonz-web-greencently@schmonz.com>
2024-08-30 21:57:16 -0400

    Bump Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    kotlin("jvm") version "2.0.10"
+    kotlin("jvm") version "2.0.20"
     jacoco
     id("com.github.ben-manes.versions") version "0.51.0"
     id("io.gitlab.arturbosch.detekt") version "1.23.6"
