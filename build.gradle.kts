dae5b4e82b6d7eef8d9f55b2247666caf869bd5a
Amitai Schleier <schmonz-web-greencently@schmonz.com>
2024-10-05 04:45:14 -0400

    Bump junit-platform-launcher, Gradle.

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 
 dependencies {
     // JUnit 5 test discovery
-    implementation("org.junit.platform:junit-platform-launcher:1.11.0")
+    implementation("org.junit.platform:junit-platform-launcher:1.11.2")
 
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
 }
