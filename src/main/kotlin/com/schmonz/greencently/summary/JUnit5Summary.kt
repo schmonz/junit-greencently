5e4e8f254d7d5a1c643a40f7f482e45cc118ec66
Amitai Schleier <schmonz-web-greencently@schmonz.com>
2024-11-27 14:31:57 -0500

    Not done yet, add more debug output.

--- src/main/kotlin/com/schmonz/greencently/summary/JUnit5Summary.kt
+++ src/main/kotlin/com/schmonz/greencently/summary/JUnit5Summary.kt
@@ -2,13 +2,22 @@ package com.schmonz.greencently.summary
 
 import java.util.Objects
 
-class JUnit5Summary(internal val testCount: Long, internal val anyTestsGreen: Long, internal val anyTestsRed: Long) {
+class JUnit5Summary(internal val testCount: Long, internal val greenTestCount: Long, internal val redTestCount: Long) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is JUnit5Summary) return false
-        return (testCount == other.testCount && anyTestsGreen > 0 && anyTestsRed == 0L)
+
+        val thinkItsEqual = testCount == other.testCount && greenTestCount > 0 && redTestCount == 0L
+
+        if (System.getenv("GREENCENTLY_SUMMARY") !== null) {
+            System.err.println("SUMMARY: $testCount, $greenTestCount, $redTestCount")
+            System.err.println("OTHER: ${other.testCount}, ${other.greenTestCount}, ${other.redTestCount}")
+            System.err.println("EQUAL? ${thinkItsEqual}")
+        }
+
+        return thinkItsEqual
     }
 
     override fun hashCode(): Int =
-        Objects.hash(anyTestsGreen, anyTestsRed, testCount)
+        Objects.hash(greenTestCount, redTestCount, testCount)
 }
